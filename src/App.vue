06c98ae5f185d1fa75f158fb4e1ce04af3dc03cf
egdiala <dialaegwuchukwu@gmail.com>
2021-05-21 22:42:01 +0100

    completed hero landing page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,186 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img src="@/assets/logo.png" alt="" class="img-fluid" width="50" height="50">
+          <strong class="title-text">Help Stone Insurance</strong>
+        </a>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="row d-flex justify-content-between">
+        <div class="col-lg-5 col-md-12 col-sm-12 my-2">
+          <h2 class="title-text fw-bolder fs-2">Instantly schedule<br> appointments with Doctors.</h2>
+          <p class="subtitle-text">Insurance plans are supported on <strong class="highlight">Help Stone</strong>. We have a range of services and support for your personal wellbeing at no price! Just call or email for free consultation.</p>
+          <a class="btn btn-success br-15" href="tel:(872) 529-6804">Contact us<i class="bi-phone-vibrate ms-2"></i></a>
+          
+        </div>
+        <div class="col-lg-6 col-md-12 col-sm-12 my-2">
+          <div id="carouselExampleFade" class="carousel slide carousel-fade px-3" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="" class="d-block w-100 br-15">
+              </div>
+              <div class="carousel-item">
+                <img src="https://images.unsplash.com/photo-1542884748-2b87b36c6b90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80" class="d-block w-100 br-15" alt="">
+              </div>
+              <div class="carousel-item">
+                <img src="https://images.unsplash.com/photo-1576765974497-b41831d52f83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=757&q=80" class="d-block w-100 br-15" alt="...">
+              </div>
+            </div>
+            <button class="carousel-control-prev ps-3" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next pe-3" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex justify-content-center my-2"><i class="bi bi-arrow-down down fs-3"></i></div>
+      
+    </div>
+    <div class="bg-2 py-5">
+      <div class="container">
+        <div class="row d-flex justify-content-between">
+          <div class="col-lg-5 col-md-12 col-sm-12 my-2">
+            <h2 class="title-text fw-bolder fs-2">Instantly schedule<br> appointments with Doctors.</h2>
+            <p class="subtitle-text">Insurance plans are supported on <strong class="highlight">Help Stone</strong>. We have a range of services and support for your personal wellbeing at no price! Just call or email for free consultation.</p>
+            <a class="btn btn-success br-15" href="tel:(872) 529-6804">Contact us<i class="bi-phone-vibrate ms-2"></i></a>
+            
+          </div>
+          <div class="col-lg-6 col-md-12 col-sm-12 my-2">
+            <div id="carouselExampleFade" class="carousel slide carousel-fade px-3" data-bs-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="" class="d-block w-100 br-15">
+                </div>
+                <div class="carousel-item">
+                  <img src="https://images.unsplash.com/photo-1542884748-2b87b36c6b90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80" class="d-block w-100 br-15" alt="">
+                </div>
+                <div class="carousel-item">
+                  <img src="https://images.unsplash.com/photo-1576765974497-b41831d52f83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=757&q=80" class="d-block w-100 br-15" alt="...">
+                </div>
+              </div>
+              <button class="carousel-control-prev ps-3" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next pe-3" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>      
+    </div>
+
+    <!-- Footer -->
+    <footer class="text-center text-lg-start bg-light text-muted">
+      <!-- Section: Social media -->
+      <section
+        class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+      >
+        <!-- Left -->
+        <div class="me-5 d-none d-lg-block">
+          <span>Get connected with us on social networks:</span>
+        </div>
+        <!-- Left -->
+
+        <!-- Right -->
+        <div>
+          <a href="https://www.facebook.com/Helpstone-Insurance-100615752229210/" target="_blank" class="me-4">
+            <i class="bi bi-facebook"></i>
+          </a>
+          <a href="" class="me-4">
+            <i class="bi bi-twitter"></i>
+          </a>
+          <a href="" class="me-4">
+            <i class="bi bi-linkedin"></i>
+          </a>
+        </div>
+        <!-- Right -->
+      </section>
+      <!-- Section: Social media -->
+
+      <!-- Section: Links  -->
+      <section class="">
+        <div class="container text-center text-md-start mt-5">
+          <!-- Grid row -->
+          <div class="row mt-3">
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-5 col-xl-4 mx-auto mb-4">
+              <!-- Content -->
+              <h6 class="text-uppercase fw-bold mb-4">
+                <img src="@/assets/logo.png" alt="" class="img-fluid" width="80" height="80">Help Stone Insurance
+              </h6>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">
+                Services
+              </h6>
+              <p>
+                <a href="#!" class="text-reset">Medicare</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Health</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Life</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Medication</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Therapy Management</a>
+              </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">
+                Contact
+              </h6>
+              <p><i class="bi bi-geo-alt-fill me-2"></i> Kokomo, Indiana, US</p>
+              <p>
+                <i class="bi bi-envelope-fill me-2"></i>
+                helpersstone@gmail.com
+              </p>
+              <p><i class="bi bi-phone-fill me-2"></i>(872) 529-6804</p>
+            </div>
+            <!-- Grid column -->
+          </div>
+          <!-- Grid row -->
+        </div>
+      </section>
+      <!-- Section: Links  -->
+
+      <!-- Copyright -->
+      <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+      Copyright © {{ new Date().getFullYear() }} Help Stone Insurance - All Rights Reserved.
+      </div>
+      <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
