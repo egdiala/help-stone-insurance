ea67202770f60b5a2389989d43d83d9dcc9d7de5
egdiala <dialaegwuchukwu@gmail.com>
2021-05-22 01:05:13 +0100

    completed help stone

--- src/App.vue
+++ src/App.vue
@@ -8,168 +8,52 @@
         </a>
       </div>
     </nav>
-    <div class="container">
-      <div class="row d-flex justify-content-between">
-        <div class="col-lg-5 col-md-12 col-sm-12 my-2">
-          <h2 class="title-text fw-bolder fs-2">Instantly schedule<br> appointments with Doctors.</h2>
-          <p class="subtitle-text">Insurance plans are supported on <strong class="highlight">Help Stone</strong>. We have a range of services and support for your personal wellbeing at no price! Just call or email for free consultation.</p>
-          <a class="btn btn-success br-15" href="tel:(872) 529-6804">Contact us<i class="bi-phone-vibrate ms-2"></i></a>
-          
-        </div>
-        <div class="col-lg-6 col-md-12 col-sm-12 my-2">
-          <div id="carouselExampleFade" class="carousel slide carousel-fade px-3" data-bs-ride="carousel">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="" class="d-block w-100 br-15">
-              </div>
-              <div class="carousel-item">
-                <img src="https://images.unsplash.com/photo-1542884748-2b87b36c6b90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80" class="d-block w-100 br-15" alt="">
-              </div>
-              <div class="carousel-item">
-                <img src="https://images.unsplash.com/photo-1576765974497-b41831d52f83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=757&q=80" class="d-block w-100 br-15" alt="...">
-              </div>
-            </div>
-            <button class="carousel-control-prev ps-3" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next pe-3" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div class="d-flex justify-content-center my-2"><i class="bi bi-arrow-down down fs-3"></i></div>
-      
-    </div>
-    <div class="bg-2 py-5">
-      <div class="container">
-        <div class="row d-flex justify-content-between">
-          <div class="col-lg-5 col-md-12 col-sm-12 my-2">
-            <h2 class="title-text fw-bolder fs-2">Instantly schedule<br> appointments with Doctors.</h2>
-            <p class="subtitle-text">Insurance plans are supported on <strong class="highlight">Help Stone</strong>. We have a range of services and support for your personal wellbeing at no price! Just call or email for free consultation.</p>
-            <a class="btn btn-success br-15" href="tel:(872) 529-6804">Contact us<i class="bi-phone-vibrate ms-2"></i></a>
-            
-          </div>
-          <div class="col-lg-6 col-md-12 col-sm-12 my-2">
-            <div id="carouselExampleFade" class="carousel slide carousel-fade px-3" data-bs-ride="carousel">
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img src="https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="" class="d-block w-100 br-15">
-                </div>
-                <div class="carousel-item">
-                  <img src="https://images.unsplash.com/photo-1542884748-2b87b36c6b90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80" class="d-block w-100 br-15" alt="">
-                </div>
-                <div class="carousel-item">
-                  <img src="https://images.unsplash.com/photo-1576765974497-b41831d52f83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=757&q=80" class="d-block w-100 br-15" alt="...">
-                </div>
-              </div>
-              <button class="carousel-control-prev ps-3" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-              </button>
-              <button class="carousel-control-next pe-3" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>      
-    </div>
-
+    <router-view></router-view>
     <!-- Footer -->
     <footer class="text-center text-lg-start bg-light text-muted">
-      <!-- Section: Social media -->
-      <section
-        class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
-      >
-        <!-- Left -->
-        <div class="me-5 d-none d-lg-block">
-          <span>Get connected with us on social networks:</span>
-        </div>
-        <!-- Left -->
-
-        <!-- Right -->
-        <div>
-          <a href="https://www.facebook.com/Helpstone-Insurance-100615752229210/" target="_blank" class="me-4">
-            <i class="bi bi-facebook"></i>
-          </a>
-          <a href="" class="me-4">
-            <i class="bi bi-twitter"></i>
-          </a>
-          <a href="" class="me-4">
-            <i class="bi bi-linkedin"></i>
-          </a>
-        </div>
-        <!-- Right -->
-      </section>
-      <!-- Section: Social media -->
-
-      <!-- Section: Links  -->
       <section class="">
         <div class="container text-center text-md-start mt-5">
-          <!-- Grid row -->
-          <div class="row mt-3">
-            <!-- Grid column -->
-            <div class="col-md-4 col-lg-5 col-xl-4 mx-auto mb-4">
-              <!-- Content -->
+          <div class="row pt-3">
+            <div class="col-md-4 col-lg-5 col-xl-4 mx-auto text-start mb-4">
               <h6 class="text-uppercase fw-bold mb-4">
                 <img src="@/assets/logo.png" alt="" class="img-fluid" width="80" height="80">Help Stone Insurance
               </h6>
             </div>
-            <!-- Grid column -->
-
-            <!-- Grid column -->
-            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-4">
-              <!-- Links -->
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto text-start ps-4 mb-4">
               <h6 class="text-uppercase fw-bold mb-4">
                 Services
               </h6>
               <p>
-                <a href="#!" class="text-reset">Medicare</a>
+                <a class="text-reset">Medicare</a>
               </p>
               <p>
-                <a href="#!" class="text-reset">Health</a>
+                <a class="text-reset">Health</a>
               </p>
               <p>
-                <a href="#!" class="text-reset">Life</a>
+                <a class="text-reset">Life</a>
               </p>
               <p>
-                <a href="#!" class="text-reset">Medication</a>
-              </p>
-              <p>
-                <a href="#!" class="text-reset">Therapy Management</a>
+                <a class="text-reset">Medication Therapy Management</a>
               </p>
             </div>
-            <!-- Grid column -->
-
-            <!-- Grid column -->
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-              <!-- Links -->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto text-start ps-4 mb-4">
               <h6 class="text-uppercase fw-bold mb-4">
                 Contact
               </h6>
               <p><i class="bi bi-geo-alt-fill me-2"></i> Kokomo, Indiana, US</p>
               <p>
-                <i class="bi bi-envelope-fill me-2"></i>
-                helpersstone@gmail.com
+                <a href="mailto:helpersstone@gmail.com" class="link"><i class="bi bi-envelope-fill me-2"></i>
+                helpersstone@gmail.com</a>
               </p>
-              <p><i class="bi bi-phone-fill me-2"></i>(872) 529-6804</p>
+              <p><a href="tel:(872) 529-6804" class="link"><i class="bi bi-phone-fill me-2"></i>(872) 529-6804</a></p>
+              <p><a href="https://www.facebook.com/Helpstone-Insurance-100615752229210/" target="_blank" class="link"><i class="bi bi-facebook me-2"></i>Help Stone Insurance</a></p>
             </div>
-            <!-- Grid column -->
           </div>
-          <!-- Grid row -->
         </div>
       </section>
-      <!-- Section: Links  -->
-
-      <!-- Copyright -->
       <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
       Copyright © {{ new Date().getFullYear() }} Help Stone Insurance - All Rights Reserved.
       </div>
-      <!-- Copyright -->
     </footer>
     <!-- Footer -->
   </div>
---
+++ src/views/Home.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <div class="row d-flex justify-content-between">
+        <div class="col-lg-5 col-md-12 col-sm-12 ps-4 my-2">
+          <h2 class="title-text fw-bolder fs-2">Instantly schedule<br> appointments with Doctors.</h2>
+          <p class="subtitle-text">Insurance plans are supported on <strong class="highlight">Help Stone</strong>. We have a range of services and support for your personal wellbeing at no price! Just call or email for free consultation.</p>
+          <a class="btn btn-success br-15" href="tel:(872) 529-6804">Contact us<i class="bi-phone-vibrate ms-2"></i></a>
+          
+        </div>
+        <div class="col-lg-6 col-md-12 col-sm-12 my-2">
+          <div id="carouselExampleFade" class="carousel slide carousel-fade px-3" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="" class="d-block w-100 br-15">
+              </div>
+              <div class="carousel-item">
+                <img src="https://images.unsplash.com/photo-1542884748-2b87b36c6b90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80" class="d-block w-100 br-15" alt="">
+              </div>
+              <div class="carousel-item">
+                <img src="https://images.unsplash.com/photo-1576765974497-b41831d52f83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=757&q=80" class="d-block w-100 br-15" alt="...">
+              </div>
+            </div>
+            <button class="carousel-control-prev ps-3" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next pe-3" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex justify-content-center my-2"><i class="bi bi-arrow-down down fs-3"></i></div>
+      
+    </div>
+    <div class="bg-2 py-5">
+      <h2 class="text-center fw-bold pb-3">Our Services</h2>
+      <div class="container">
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+          <div v-for="(service, s) in services" :key="s" class="col">
+            <div class="card br-15 h-100">
+              <img :src="service.img" class="card-img-top br-15" alt="">
+              <div class="card-body">
+                <h5 class="card-title">{{service.title}}</h5>
+                <p class="card-text">{{service.text}}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>      
+    </div>
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-md-8 mx-auto">
+          <h2 class="text-center fw-bold pb-3">Schedule an appointment</h2>
+          <form name="contact"
+              method="POST"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field" class="row g-3">
+              <input type="hidden" name="form-name" value="contact" />
+            <div class="col-md-6">
+              <input type="text" name="name" class="form-control" placeholder="Name" required>
+            </div>
+            <div class="col-md-6">
+              <input type="email" name="email" class="form-control" placeholder="Email" required>
+            </div>
+            <div class="col-md-6">
+              <input type="text" name="phone" class="form-control" placeholder="Phone" required>
+            </div>
+            <div class="col-md-6">
+              <select name="service" class="form-select" id="autoSizingSelect" required>
+                <option value="Medicare">Medicare</option>
+                <option value="Health">Health</option>
+                <option value="Life">Life</option>
+                <option value="Medication Therapy Management">Medication Therapy Management</option>
+              </select>
+            </div>
+            <div class="col-md-12">
+              <textarea name="message" class="form-control" placeholder="Leave a message here" rows="5"></textarea>
+            </div>
+            <div class="d-grid gap-2 col-md-6 mx-auto">
+              <button class="btn btn-success br-15 px-3 float-end">Send<i class="bi-chevron-right ms-2"></i></button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      services: [
+        {
+          img: 'https://images.unsplash.com/photo-1601582037073-b36af0b8ea81?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+          title: 'Medicare',
+          text: 'A Medicare Supplement Insurance (Medigap) policy, sold by private companies, can help pay some of the health care costs that Original Medicare doesn’t, like co-payments, co-insurance, and deductibles.'
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1612277795054-a7a98257dd6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+          title: 'Health',
+          text: 'This is a short card.'
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1587854692152-cbe660dbde88?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+          title: 'Medication Therapy Management',
+          text: 'This is a longer card with supporting text below as a natural lead-in to additional content.'
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1554331292-735256644d5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=755&q=80',
+          title: 'Life',
+          text: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+        }
+      ]
+    }
+  },
+}
+</script>
